98609d0705432443c66e25c5b1d4cb7ad1a78fb3
Siddhant Chinchkar <siddhantchinchkar2023.comp@mmcoe.edu.in>
2026-09-07 22:13:15 +0530

    Add Data Types program

---
+++ Programs/DataTypesinjava/userDetails.java
@@ -0,0 +1,25 @@
+package DataTypesinjava;
+
+public class userDetails {
+
+    public static void main(String[] args) {
+
+        byte RollNo = 101;
+        short Id = 1234;
+        int PinCode = 897570;
+        long MobileNo = 9371693853L;
+        float Marks = 96.32f;
+        double Salary = 40000;
+        boolean IsSelected = true;
+        char Gender = 'M';
+
+        System.out.println("RollNo is    =" + RollNo);
+        System.out.println("ID is        =" + Id);
+        System.out.println("Pincode is   =" + PinCode);
+        System.out.println("Mobile No is =" + MobileNo);
+        System.out.println("Marks is     =" + Marks);
+        System.out.println("Salary  is   =" + Salary);
+        System.out.println("Selected is  =" + IsSelected);
+        System.out.println("Gender  is   =" + Gender);
+    }
+}
